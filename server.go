bc15e7400b16c71cabecad699859961f61f3d8a0
kristovish <info.torresdesantander@gmai.com>
2023-01-15 16:07:48 -0300

    added port :v

--- server.go
+++ server.go
@@ -4,13 +4,29 @@ import (
     "fmt"
     "log"
     "net/http"
+    "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Saludos, bienvenido a %s!", r.URL.Path[1:])
+    // Log the request
+    log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+
+    // Write the response
+    fmt.Fprintf(w, "Bienvenidos a Moonlight Falls!")
 }
 
 func main() {
+    // Create a log file
+    f, err := os.OpenFile("moonlight.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatalf("Error opening log file: %v", err)
+    }
+    defer f.Close()
+
+    // Use the log file for logging
+    log.SetOutput(f)
+
+    // Handle requests
     http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
